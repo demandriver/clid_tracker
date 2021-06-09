25bee0cc97ef5b852b0d2adf555b2d4e94bd32b7
demandriver <82829702+demandriver@users.noreply.github.com>
2021-06-09 10:56:11 -1200

    Update source_handler.js

--- source_handler.js
+++ source_handler.js
@@ -1,99 +1,127 @@
 drSources = function(){
-  
   //initialize vars to be used inside the function
+    
   var url;
+
+  var url_sources_param_key = 'source';
+  var url_sources_param;
   var url_sources;
+
+  var stored_sources_cookie_name = 'dr_sources';
+  var stored_sources_cookie;
+  var stored_sources;
+
+  var agency_param_key = 'agency';
+  var dr_agency_param_value = 'demandriver';
   var agency_param;
-  
-  var cookie_name;
-  var cookie_sources;
-  var cookie_duration;
-  
+
+  var last_touch_param_key = 'dr_lt';
+  var last_touch_param;
+  var last_touch_cookie_name = 'dr_lt';
+  var last_touch_cookie;
+  var last_touch;
+
   var all_sources;
-  
+
+  var cookie_duration;
   var time;
   var now;
-  var future_cutoff;
-  var past_cutoff;
-  
-  function init(cookie = 'dr_sources', duration = 30*24*60*60*1000, debug = false){
-    //assign vars
+  var expiration_time;
+
+  const getCookie = (name) => (
+    document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || undefined
+  )
+
+  function getCurrentStateVars(){
     url = new URL(window.location.href);
-    agency_param = url.searchParams.get('agency');
-    if(agency_param == 'demandriver'){
-      let source_param = url.searchParams.get('source'); //strip of source param from url
-      if(source_param){url_sources = source_param.split('-');} //if you found a source param, store as array in url_sources
-    }
-    
-    cookie_name = cookie;
-    cookie_sources = storedSources(); //get stored sources from cookie
-    cookie_duration = duration;
-
-    all_sources = getAllSources(); //combine stored sources and url sources
-
+    url_sources_param = url.searchParams.get(url_sources_param_key); //strip off source param from url
+    stored_sources_cookie = getCookie(stored_sources_cookie_name);
+    agency_param = url.searchParams.get(agency_param_key);
+    last_touch_param = parseInt(url.searchParams.get(last_touch_param_key));
+    last_touch_cookie = parseInt(getCookie(last_touch_cookie_name));
     time = new Date();
     now = time.getTime();
-    future_cutoff = now + cookie_duration;
+  }  
 
-    saveSources();
-    addSourceParam();
+  function getAllSources(){ //ok
+    if(agency_param == dr_agency_param_value || last_touch_param){
+      if(url_sources_param){url_sources = url_sources_param.split('-');} //if you found a source param in url, store as array in url_sources
+    }
+    if(stored_sources_cookie){stored_sources = stored_sources_cookie.split('-');} //get stored sources from cookie
+    if(url_sources && stored_sources){
+      all_sources = [...new Set(url_sources.concat(stored_sources))];
+    }else if(url_sources){
+      all_sources = [...new Set(url_sources)];
+    }else if(stored_sources){
+      all_sources = [...new Set(stored_sources)];
+    }
+  }
+
+  function getLastTouch(){ //ok
+    //last touch is now if agency=demandriver present. otherwise, last touch is latest of url param and cookie
+    if(agency_param == dr_agency_param_value){
+      last_touch = now;
+    }else if(last_touch_param && last_touch_cookie){
+      last_touch = Math.max(last_touch_param, last_touch_cookie);
+    }else if(last_touch_param){
+      last_touch = last_touch_param;
+    }else if (last_touch_cookie){
+      last_touch = last_touch_cookie;
+    }     
+  }
+
+  function saveCookies(){ //ok
+    if(all_sources){
+      let cookie_str = '';
+      expiration_time = last_touch + cookie_duration;
+      time.setTime(expiration_time); //set expiration date
+      cookie_str = stored_sources_cookie_name + '=' + all_sources.join('-') + ';expires=' + time.toUTCString() + ';path=/';
+      document.cookie = cookie_str;
+      cookie_str = last_touch_cookie_name + '=' + last_touch.toString() + ';expires=' + time.toUTCString() + ';path=/';
+      document.cookie = cookie_str;
+    }
+  }
+
+  function addParams(){ //ok
+    if(all_sources){
+      url.searchParams.set(last_touch_param_key,last_touch.toString());
+      url.searchParams.set(url_sources_param_key,all_sources.join('-'));
+      window.history.replaceState(null, null, url.href);
+    }
+  }
+
+  function init(duration = 30*24*60*60*1000, debug = false){
+    cookie_duration = duration;
+    getCurrentStateVars(); //get params and cookies and init URL(), Date(), etc.
+    getAllSources(); //combine stored sources and url sources
+    getLastTouch();
+    saveCookies();
+    addParams();
+
+    setInterval(function(){
+      if(window.location.href != url.href){
+        url.href = window.location.href;
+        addParams();
+      }
+    }, 50);
+
 
     if(debug){
       console.log("drSource vars:");
       console.log(url_sources);
-      console.log(cookie_sources);
+      console.log(stored_sources);
       console.log(all_sources);
-    }
+      }
   }
-  
-  function storedSources(){
-    const getCookie = (name) => (
-      document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || undefined
-    )
-    let cookie = getCookie(cookie_name);
-    if(cookie){
-      return cookie.split('-');
-    }else{
-      return null;
-    }
-  }
-  
-  function getAllSources(){
-    if(url_sources && cookie_sources){
-      return [...new Set(url_sources.concat(cookie_sources))];
-    }else if(url_sources){
-      return [...new Set(url_sources)];
-    }else if(cookie_sources){
-      return [...new Set(cookie_sources)];
-    }else{
-      return null;
-    }
-  }
-  
-  function saveSources(){
-    if(all_sources){
-      time.setTime(future_cutoff); //set expiration date
-      let cookie_str = cookie_name + '=' + all_sources.join('-') + ';expires=' + time.toUTCString() + ';path=/';
-      document.cookie = cookie_str;
-    }
-  }
-  
-  function addSourceParam(){
-    if(all_sources){
-      url.searchParams.set('agency','demandriver');
-      url.searchParams.set('source',all_sources.join('-'));
-      window.history.replaceState(null, null, url.href);
-    }
-  }
-  
+
   function sendSources(endpoint,data = ''){
-    if(all_sources){
+    if(all_sources && last_touch){
       var xhr = new XMLHttpRequest();
       xhr.open("POST", endpoint, true);
-      xhr.send(JSON.stringify({'dr_sources':all_sources,'data':data}));
+      xhr.send(JSON.stringify({'dr_sources':all_sources,'last_touch':last_touch,'data':data}));
     }
   }
-  
+
   return{
     init:init,
     send:sendSources
